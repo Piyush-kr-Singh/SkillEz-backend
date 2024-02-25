0b990d1720afe4d3fd1f77e2f3bd05c9adbdc43a
Piyush <raunaksingh4099@gmail.com>
2024-02-26 01:13:25 +0530

    All cors added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectDb = require("./utils/db");
+const errorMiddleWare = require('./middlewares/error-middleware');
 const authRouter = require("./router/auth-router");
 const contactRoute = require('./router/contact-router');
 const serviceRoute = require('./router/service-router');
@@ -12,26 +14,16 @@ const resumeReviewFormRoute = require("./router/resume-review-router");
 const resumeBuildFormRoute = require("./router/resume-build-router");
 const mockInterviewFormRoute = require("./router/mockInterview-router");
 const newsLetterRoute = require("./router/newsLetter-router");
+// const forgotPasswordRoute = require('./router/forgotPassword-router');
 
-const connectDb = require("./utils/db");
-const errorMiddleWare = require('./middlewares/error-middleware');
-const PORT = 5000;
-
-// Update the origin to 'https://skillez.in'
-// const corsOptions = {
-//     origin: "https://www.skillez.in", // Update the origin to the correct domain
-//     methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
-//     credentials: true
-// };
-// app.use(cors(corsOptions));
-
-
+const PORT = process.env.PORT || 5000; // Use the provided port or 5000 as a default
 
 const corsOptions = {
     origin: "*", // Allow requests from any origin
     methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
     credentials: true
 };
+
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
@@ -53,9 +45,12 @@ app.use("/api/service", mockInterviewFormRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/admin", adminContactRoute);
 
-app.use(errorMiddleWare);
-
 app.use("/api/newsletter", newsLetterRoute);
+
+// Add the route for forgot password
+// app.use("/api/forgotpassword", forgotPasswordRoute);
+
+app.use(errorMiddleWare);
 
 connectDb()
     .then(() => {
